b60d50ca73e27661546df30dfa840a06252df3c5
Jayesh Kitukale <jayeshkitukale@Jayeshs-MacBook-Pro.local>
2025-05-21 19:39:17 +0530

    Update layout and page components, add UI components and tailwind config

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,12 +15,12 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
+            <body className="antialiased text-white">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
+                    <div className="flex flex-col w-full max-w-12xl mx-auto grow">
+                        {/* <Header /> */}
                         <main className="grow">{children}</main>
-                        <Footer />
+                        {/* <Footer /> */}
                     </div>
                 </div>
             </body>
